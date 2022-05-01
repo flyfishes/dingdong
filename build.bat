f9d8aa239a274f8c8959bd93b36602cee32e6443
allen <allenzh77@gmail.com>
2022-05-01 16:43:35 +0800

    Merge branch 'main' of https://github.com/gelove/dingdong

---
+++ build.bat
@@ -0,0 +1,5 @@
+e:
+cd E:\app\dingdong\dingdong
+go build -o C:\Users\zhoux\Desktop\dingdong-windows-amd64\dingdong.exe cmd\dingdong\main.go
+c:
+
--- internal/app/app.go
+++ internal/app/app.go
@@ -10,6 +10,7 @@ import (
 	"dingdong/internal/app/api"
 	"dingdong/internal/app/config"
 	"dingdong/internal/app/service"
+	"dingdong/internal/app/service/notify"
 )
 
 func Run() {
@@ -27,6 +28,17 @@ func Run() {
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.FS(assets.FS))))
 
 	conf := config.Get()
+	if len(conf.Bark) > 0 {
+		for _, v := range conf.Bark {
+			notify.Push(v, "start run dingdong helper")
+		}
+	}
+	if len(conf.PushPlus) > 0 {
+		for _, v := range conf.PushPlus {
+			notify.PushPlus(v, "start run dingdong helper")
+		}
+	}
+
 	err := http.ListenAndServe(conf.Addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe => ", err)
@@ -41,10 +53,10 @@ func isPeak() bool {
 	if now.Hour() == 6 && now.Minute() < 10 {
 		return true
 	}
-	if now.Hour() == 8 && now.Minute() < 40 {
+	if now.Hour() == 8 && now.Minute() < 41 && now.Minute() > 20 {
 		return true
 	}
-	if now.Hour() >= 22 {
+	if now.Hour() >= 19 {
 		return true
 	}
 	return false
@@ -54,22 +66,34 @@ func isPeak() bool {
 func Monitor() {
 	cartMap := service.MockCartMap()
 	for {
-		//<-time.After(time.Second) ////定时1s，阻塞1s,1s后产生一个事件，往channel写内容
 		conf := config.Get()
-		if !conf.MonitorNeeded && !conf.PickUpNeeded {
-			continue
-		}
-		/* // 每5分钟第1秒运行一次
-		now := time.Now()
-		if now.Second() != 0 {
-			continue
-		} */
-		if isPeak() {
-			log.Println("当前高峰期或暂未营业")
-			continue
-		}
-		service.MonitorAndPickUp(cartMap)
 		duration := conf.MonitorIntervalMin + rand.Intn(conf.MonitorIntervalMax-conf.MonitorIntervalMin)
-		<-time.After(time.Duration(duration) * time.Second)
+		bMonitored := 0 // 二进制最右1位区分监听，其他几位区分高峰期
+		for i := 0; i < duration/5; i++ {
+			<-time.After(time.Duration(5) * time.Second)
+			//<-time.After(time.Second) ////定时1s，阻塞1s,1s后产生一个事件，往channel写内容
+			if !conf.MonitorNeeded && !conf.PickUpNeeded {
+				continue
+			}
+			/* // 每5分钟第1秒运行一次
+			now := time.Now()
+			if now.Second() != 0 {
+				continue
+			} */
+			if isPeak() {
+				if bMonitored < 2 {
+					log.Println("当前高峰期或暂未营业.")
+				}
+				bMonitored += 2
+				continue
+			}
+			if bMonitored%2 == 0 {
+				bMonitored += 1
+				service.MonitorAndPickUp(cartMap)
+				//				_ = bMonitored
+			}
+		}
+
+		//<-time.After(time.Duration(duration) * time.Second)
 	}
 }
